5eb0ee7e80258d6f6b286ca998ffa69bfc48d8c4
sumpf <sumpf@hanmail.net>
2024-12-28 16:39:23 +0900

    Revert "add info java file"
    This reverts commit d014af9ca2ed5442d4a5e64dd4b025544235fcef.

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!!!, Nice to meet you";
+    private final String message = "Hello WWorld!!!";
 
     public App() {}
 
